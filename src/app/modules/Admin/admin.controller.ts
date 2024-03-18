2c11861af4f25e2b6047cede2eb55c1bde098b19
JannatunNimeNishat <nishatnime100@gmail.com>
2024-03-19 03:55:31 +0600

    admin constant file added

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,16 +1,12 @@
 import { Request, Response } from "express";
 import { adminService } from "./admin.service";
 import pick from "../../../shared/pick";
+import { adminFilterableFields } from "./admin.constant";
 
 const getAllAdmin = async (req: Request, res: Response) => {
   try {
     //amader tik kore deya field er bahirer field gulo asle jate pick na kore, saijonno req.query ta ke filter kore neya hosce pick function deya
-    const filters = pick(req.query, [
-      "name",
-      "email",
-      "searchTerm",
-      "contactNumber",
-    ]);
+    const filters = pick(req.query, adminFilterableFields);
 
     const result = await adminService.getAdminFromDB(filters);
 
