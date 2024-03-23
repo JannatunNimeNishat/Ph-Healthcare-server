6c837c37111275bc2308590c7d9a7584ec6068f7
JannatunNimeNishat <nishatnime100@gmail.com>
2024-03-24 01:13:58 +0600

    validateRequest middleware added

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,13 +1,23 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { adminController } from "./admin.controller";
-
+import z, { AnyZodObject } from "zod";
+import validateRequest from "../../middleWares/validateRequest";
 const router = Router();
+
+const update = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    contactNumber: z.string().optional(),
+  }),
+});
+
+
 
 router.get("/", adminController.getAllAdmin);
 
 router.get("/:id", adminController.getById);
 
-router.patch("/:id", adminController.updateAdmin);
+router.patch("/:id", validateRequest(update), adminController.updateAdmin);
 
 router.delete("/:id", adminController.deleteAdmin);
 
