0eb1c5173530b1ed596461ba30e901ff69411526
JannatunNimeNishat <nishatnime100@gmail.com>
2024-03-25 22:31:09 +0600

    updated

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -6,15 +6,23 @@ import httpStatus from "http-status";
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const result = await authServices.loginUserIntoDB(req.body);
+  // setting the refresh token to cookies
+  const {refreshToken}= result;
+  res.cookie('refreshToken',refreshToken,{
+    secure:false, //true when it is no the production
+    httpOnly:true
+  })
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: "Logged in successfully",
-    data: result,
+    data: {
+      accessToken:result.accessToken,
+      needPasswordChange:result.needPasswordChange
+    },
   });
 });
 
-
 export const authController = {
-    loginUser
-}
+  loginUser,
+};
--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,8 +1,53 @@
+import prisma from "../../../shared/prisma";
+import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const loginUserIntoDB = async (payload: {
   email: string;
   password: string;
 }) => {
-  console.log(payload);
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: payload.email,
+    },
+  });
+  const isCorrectPassword: boolean = await bcrypt.compare(
+    payload.password,
+    userData.password
+  );
+
+  if (!isCorrectPassword) {
+    throw new Error("Password incorrect!");
+  }
+
+  const accessToken = jwt.sign(
+    {
+      email: userData.email,
+      role: userData.role,
+    },
+    "abcdefg",
+    {
+      algorithm: "HS256",
+      expiresIn: "5m",
+    }
+  );
+
+  const refreshToken = jwt.sign(
+    {
+      email: userData.email,
+      role: userData.role,
+    },
+    "hijklmn",
+    {
+      algorithm: "HS256",
+      expiresIn: "30d",
+    }
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+    needPasswordChange: userData.needPasswordChange,
+  };
 };
 
 export const authServices = {
