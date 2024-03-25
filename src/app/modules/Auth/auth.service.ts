c28974ed0f46e220119dcb954986d95528814f28
JannatunNimeNishat <nishatnime100@gmail.com>
2024-03-26 00:15:20 +0600

    updated

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,7 +1,8 @@
+import { UserStatus } from "@prisma/client";
 import { jwtHelpers } from "../../../helpers/jwtHelpers";
 import prisma from "../../../shared/prisma";
 import * as bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+
 const loginUserIntoDB = async (payload: {
   email: string;
   password: string;
@@ -9,6 +10,7 @@ const loginUserIntoDB = async (payload: {
   const userData = await prisma.user.findUniqueOrThrow({
     where: {
       email: payload.email,
+      status: UserStatus.ACTIVE // user jodi deleted/blocked na hoy. delete/blocked hoa user ke login korte dibo na
     },
   });
   const isCorrectPassword: boolean = await bcrypt.compare(
@@ -48,19 +50,18 @@ const loginUserIntoDB = async (payload: {
 const generatingAccessTokenFromRefreshTokenIntoDB = async (token: string) => {
   let decodedData;
   try {
-    decodedData = jwt.verify(token, "abcdefgijkmnop");
-    
+    decodedData = jwtHelpers.verifyToken(token, "abcdefgijkmnop");
   } catch (error) {
     throw new Error("You are not authorized");
   }
   // checking the decoded user email from token is exists or not. If not then it will throw an
   //error from here
   const userData = await prisma.user.findUniqueOrThrow({
-    where:{
-      email:decodedData?.email
-    }
+    where: {
+      email: decodedData?.email,
+      status: UserStatus.ACTIVE // user jodi deleted/blocked na hoy. delete/blocked hoa user ke token dibo na
+    },
   });
-
 
   //generating the new accessToken
   const accessToken = jwtHelpers.generateToken(
@@ -73,7 +74,6 @@ const generatingAccessTokenFromRefreshTokenIntoDB = async (token: string) => {
   );
 
   return accessToken;
-
 };
 
 export const authServices = {
