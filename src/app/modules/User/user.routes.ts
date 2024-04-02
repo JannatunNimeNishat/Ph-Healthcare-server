04f5c1f01a994dd422dd76027750f9b0ccfcb160
JannatunNimeNishat <nishatnime100@gmail.com>
2024-04-03 03:20:10 +0600

    Updated

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -9,7 +9,7 @@ import { userValidation } from "./user.validation";
 const router = Router();
 
 router.post(
-  "/",
+  "/create-admin",
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   fileUploader.upload.single("file"),
   (req: Request, res: Response, next: NextFunction) => {
