a630dc2a55e52c29246bbb06886c028d8bc619db
JannatunNimeNishat <nishatnime100@gmail.com>
2024-04-16 00:30:13 +0600

    createDoctorSchedule api added

---
+++ src/app/modules/DoctorSchedule/doctorSchedule.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import httpStatus from "http-status";
+import { DoctorScheduleService } from "./doctorSchedule.service";
+import { IAuthUser } from "../../interfaces/common";
+
+const createDoctorSchedule = catchAsync(async (req: Request & {user?:IAuthUser},  res: Response) => {
+    const user = req.user;
+  
+    const result = await DoctorScheduleService.createDoctorScheduleIntoDB(user,req.body);
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Doctor schedule created successfully",
+      data: result
+    });
+  });
+
+
+  export const DoctorScheduleController = {
+    createDoctorSchedule
+  }
